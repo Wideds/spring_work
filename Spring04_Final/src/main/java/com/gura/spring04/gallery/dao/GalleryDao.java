9f4078205fecc029c4cff6d72ab2105ee9dee29f
lim hyo yeol <why4517@naver.com>
2023-07-04 15:07:18 +0900

    Spring04 에 갤러리 기능 완성함

--- Spring04_Final/src/main/java/com/gura/spring04/gallery/dao/GalleryDao.java
+++ Spring04_Final/src/main/java/com/gura/spring04/gallery/dao/GalleryDao.java
@@ -7,10 +7,10 @@ import com.gura.spring04.gallery.dto.GalleryDto;
 public interface GalleryDao {
 	//gallery 리스트 가져오기
 	public List<GalleryDto> getList(GalleryDto dto);
-	//모든 ROW의 개수
-	public int getCount(GalleryDto dto);
+	//모든 ROW 의 개수
+	public int getCount();
 	//갤러리에 사진 저장하기
 	public void insert(GalleryDto dto);
-	//pk num 에 해당하는 DB에서 gallery 게시글 하나의 data 가져오기
+	//pk num 에 해당하는 DB 에서 gallery 게시글 하나의 data 가져오기
 	public GalleryDto getData(int num);
 }
--- Spring04_Final/src/main/java/com/gura/spring04/gallery/dao/GalleryDaoImpl.java
+++ Spring04_Final/src/main/java/com/gura/spring04/gallery/dao/GalleryDaoImpl.java
@@ -10,54 +10,50 @@ import com.gura.spring04.gallery.dto.GalleryDto;
 
 @Repository
 public class GalleryDaoImpl implements GalleryDao{
-   
-   @Autowired
-   private SqlSession session;
-   
-   @Override
-   public List<GalleryDto> getList(GalleryDto dto) {
-      /*
-       * Mapper's namespace : gallery
-       * sql's id : getList
-       * parameterType : GalleryDto
-       * resultType : GalleryDto
-       */
-      return session.selectList("gallery.getList", dto);
-   }
 
-   @Override
-   public int getCount(GalleryDto dto) {
-      /*
-       * Mapper's namespace : gallery
-       * sql's id : getCount
-       * resultType : int
-       */
-      return session.selectOne("gallery.getCount");
-   }
+	@Autowired
+	private SqlSession session;
 
-   @Override
-   public void insert(GalleryDto dto) {
-      /*
-       * Mapper's namespace : gallery
-       * sql's id : insert
-       * parameterType : GalleryDto
-       */
-      session.insert("gallery.insert", dto);
-   }
+	@Override
+	public List<GalleryDto> getList(GalleryDto dto) {
+		/*
+		 * Mapper's namespace : gallery
+		 * sql's id : getList
+		 * parameterType : GalleryDto
+		 * resultType : GalleryDto
+		 */
+		return session.selectList("gallery.getList", dto);
+	}
 
-   @Override
-   public GalleryDto getData(int num) {
-      /*
-       * Mapper's namespace : gallery
-       * sql's id : getData
-       * parameterType : int
-       * resultType : GalleryDto
-       */
-      return session.selectOne("gallery.getData", num);
-   }
+	@Override
+	public int getCount() {
+		/*
+		 * Mapper's namespace : gallery
+		 * sql's id : getCount
+		 * resultType : int
+		 */
+		return session.selectOne("gallery.getCount");
+	}
+
+	@Override
+	public void insert(GalleryDto dto) {
+		/*
+		 * Mapper's namespace : gallery
+		 * sql's id : insert
+		 * parameterType : GalleryDto
+		 */
+		session.insert("gallery.insert", dto);
+	}
+
+	@Override
+	public GalleryDto getData(int num) {
+		/*
+		 * Mapper's namespace : gallery
+		 * sql's id : getData
+		 * parameterType : int
+		 * resultType : GalleryDto
+		 */
+		return session.selectOne("gallery.getData", num);
+	}
 
 }
-
-
-
-
