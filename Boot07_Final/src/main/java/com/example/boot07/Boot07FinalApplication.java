b9dc63205e613dca78c7329e10e235b0fb7db2b2
lim hyo yeol <why4517@naver.com>
2023-07-13 15:48:44 +0900

    Boot07 에 users 기능 추가,users 관련기능 spring boot 에 맞게 수정

---
+++ Boot07_Final/src/main/java/com/example/boot07/Boot07FinalApplication.java
@@ -0,0 +1,16 @@
+package com.example.boot07;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot07FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot07FinalApplication.class, args);
+
+	}
+
+}
